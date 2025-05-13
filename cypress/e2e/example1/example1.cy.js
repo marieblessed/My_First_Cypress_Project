dc503af584332575d70d377fa8549f18c8f79ebb
marieblessed <mmusiment@gmail.com>
2025-05-13 15:00:21 +0300

    Added  PIM Access

--- cypress/e2e/example1/example1.cy.js
+++ cypress/e2e/example1/example1.cy.js
@@ -1,13 +1,33 @@
 /// <reference types="cypress" />
-describe("visit OrangeHRM", ()=>{
-    it("visit main url",()=>{
-        cy.visit("https://opensource-demo.orangehrmlive.com")
-    })
 
-    it("Login OrangeHRM", ()=>{
-        cy.visit("https://opensource-demo.orangehrmlive.com")
-        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admin")
-        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
-        cy.get('.oxd-button').click()
-    })
-})
+describe("Visit OrangeHRM", () => {
+
+    beforeEach(() => {
+      cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"); // Use the full login path to avoid redirection issues
+  
+      // Wait for the username field to be visible before typing
+      cy.get('input[name="username"]', { timeout: 10000 }).should('be.visible').type("Admin");
+      cy.get('input[name="password"]').should('be.visible').type("admin123");
+      cy.get('button[type="submit"]').click();
+  
+      // Confirm successful login
+      cy.url({ timeout: 10000 }).should('include', '/dashboard');
+    });
+  
+    it("Visit main URL", () => {
+      cy.url().should('include', '/dashboard');
+    });
+  
+    it("Login OrangeHRM", () => {
+      cy.contains('Dashboard').should('be.visible');
+    });
+  
+    it("Access PIM", () => {
+      cy.get('.oxd-sidepanel-body').should('be.visible');
+      cy.contains('.oxd-main-menu-item', 'PIM').should('be.visible').click();
+      cy.url().should('include', '/pim/viewEmployeeList');
+      cy.contains('Employee Information').should('be.visible');
+    });
+  
+  });
+  
